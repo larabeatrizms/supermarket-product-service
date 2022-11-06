ccb84f89617d6d9cf7464bbd8ec28dc534c72b09
Lara Beatriz <31808723+larabeatrizms@users.noreply.github.com>
2022-11-05 22:17:02 -0300

    feat: delete image product on azure

--- src/modules/product/services/delete-product.service.ts
+++ src/modules/product/services/delete-product.service.ts
@@ -1,6 +1,7 @@
 import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
 import { BadRequestException, Inject, Logger } from '@nestjs/common';
 import { ISuccessResponse } from 'src/shared/interfaces/SuccessResponse.interface';
+import { FilesAzureService } from 'src/shared/providers/azure-files/files-azure.service';
 import { ProductRepositoryInterface } from '../repositories/product/product.interface.repository';
 
 export class DeleteProductService {
@@ -9,19 +10,26 @@ export class DeleteProductService {
   constructor(
     @Inject('ProductRepositoryInterface')
     private readonly productRepository: ProductRepositoryInterface,
+    private readonly fileService: FilesAzureService,
     private readonly amqpConnection: AmqpConnection,
   ) {}
 
   async execute(id: number): Promise<ISuccessResponse | Error> {
     this.logger.log(`Searching product... id: ${id}`);
 
-    const product = await this.productRepository.remove(id);
+    const product = await this.productRepository.findOneById(id);
 
     if (!product) {
       this.logger.log('Product not found.');
 
       return new BadRequestException('Produto não encontrado!');
     }
+
+    const fileToRemove = product.image.split('/').pop();
+
+    await this.fileService.deleteFile(fileToRemove);
+
+    await this.productRepository.remove(id);
 
     this.logger.log('Product deleted.');
 
